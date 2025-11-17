bdb6ebf4f4b525020aa6c51309dabd39c5cc1b3b
Thomas Giovannini <thomas.giovannini@studio.unibo.it>
2025-11-17 12:21:47 +0100

    92: implemented Controller to save one file and interact with it.

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,46 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.channels.FileLockInterruptionException;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private File currFile;
 
+    public Controller() {
+        this.currFile = new File(System.getProperty("user.home")
+            + File.separator
+            + "output.txt");
+    }
+
+    public void setFile(File file) {
+        if(file.exists()) {
+            this.currFile = file;
+        } else {
+            throw new IllegalArgumentException("No file sent.");
+        }
+    }
+
+    public File getFile() {
+        return this.currFile;
+    }
+
+    public String getPath() {
+        return this.currFile.getAbsolutePath();
+    }
+
+    public void writeOnFile(String str) throws FileNotFoundException {
+        PrintStream out = new PrintStream(currFile);
+        out.print(str);
+        out.close();
+    }
 }
