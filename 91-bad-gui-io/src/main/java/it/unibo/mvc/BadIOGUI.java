a25f68dfb398b918393f919cd6a2bbabb051fd79
Thomas Giovannini <thomas.giovannini@studio.unibo.it>
2025-11-17 11:37:04 +0100

    91: implemented read from file on read button.

--- 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
+++ 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
@@ -10,11 +10,15 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Random;
 
@@ -75,7 +79,17 @@ public class BadIOGUI {
         read.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent ignored){
-                System.out.println("Hello thanks for pressing");
+                File f = new File(PATH);
+                Path p = f.toPath();
+                try (InputStream is = new FileInputStream(PATH)) {
+                    List<String> out = Files.readAllLines(p, StandardCharsets.UTF_8);
+                    for(String line : out){
+                        System.out.println(line);
+                    }
+                }catch (final IOException e) {
+                    JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace(); // NOPMD: allowed as this is just an exercise
+                }
             }
         });
     }
