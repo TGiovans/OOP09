654c14910bde89a81ad968cccb581010571e9f5f
Thomas Giovannini <thomas.giovannini@studio.unibo.it>
2025-11-18 12:21:39 +0100

    92: Implemented SimpleGUIWithFileChooser to retrieve file names by browsing and saving them in the controller.

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,16 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import it.unibo.mvc.Controller;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +19,37 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
+    private final Controller ctrl;
+
+    public SimpleGUI(){
+        ctrl = new Controller();
+        final JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        JTextArea empty = new JTextArea();
+        mainPanel.add(empty, BorderLayout.CENTER);
+        JButton save = new JButton("Save");
+        mainPanel.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        save.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent a) {
+                ctrl.setFile(ctrl.getFile());
+            }
+        });
+    }
+
+    private void display() {
+            final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+            final int sw = (int) screen.getWidth();
+            final int sh = (int) screen.getHeight();
+            frame.setSize(sw / PROPORTION, sh / PROPORTION);
+            frame.setLocationByPlatform(true);
+            frame.setVisible(true);
+        }
+        
+    public static void main(String[] args) {
+        new SimpleGUIWithFileChooser().display();
+    }
 
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,6 +1,20 @@
 package it.unibo.mvc;
 
+import it.unibo.mvc.Controller;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 
 /**
@@ -9,6 +23,54 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUIWithFileChooser {
 
+    private final Controller dealer = new Controller();
     private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
+    
+
+    public SimpleGUIWithFileChooser(){
+        final JTextField fileChooser = new JTextField();
+        final JPanel fileChoicePanel = new JPanel();
+        final JButton filePress = new JButton("Browse");
+        final JPanel mainPanel = new JPanel();
+        final JTextArea empty = new JTextArea();
+        final JButton save = new JButton("Save");
+        fileChoicePanel.setLayout(new BorderLayout());
+        mainPanel.setLayout(new BorderLayout());
+        fileChooser.setEditable(false);
+        fileChooser.setText(dealer.getPath());
+        filePress.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent a) {
+                JFileChooser browser = new JFileChooser();
+                switch(browser.showSaveDialog(null)) {
+                    case JFileChooser.APPROVE_OPTION: dealer.setFile(browser.getSelectedFile()); 
+                                                      fileChooser.setText(dealer.getPath());
+                                                      break;
+                    case JFileChooser.CANCEL_OPTION: break;
+                    default: JOptionPane.showMessageDialog(null, 
+                                                "Apologies, an error has occurred.", 
+                                                  "ERROR", 
+                                                        JOptionPane.ERROR_MESSAGE); 
+                }
+            }
+        });
+        fileChoicePanel.add(fileChooser, BorderLayout.CENTER);
+        fileChoicePanel.add(filePress, BorderLayout.EAST);
+        mainPanel.add(empty, BorderLayout.CENTER);
+        mainPanel.add(save, BorderLayout.SOUTH);
+        mainPanel.add(fileChoicePanel, BorderLayout.NORTH);
+        frame.setContentPane(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    
+    protected void display() {
+            final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+            final int sw = (int) screen.getWidth();
+            final int sh = (int) screen.getHeight();
+            frame.setSize(sw / PROPORTION, sh / PROPORTION);
+            frame.setLocationByPlatform(true);
+            frame.setVisible(true);
+        }
 
 }
