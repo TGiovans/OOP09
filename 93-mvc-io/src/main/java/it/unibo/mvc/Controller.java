0c765bc109fed89806f563194844e2db55daa23a
Thomas Giovannini <thomas.giovannini@studio.unibo.it>
2025-11-22 17:35:07 +0100

    93: constructed controller interface. Implemented Controller as requested.

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,35 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    /**
+     * Sets the next line to print as the string in input.
+     */
+    public void setNextPrint(String newstr);
+
+    /**
+     * Gets the next string the controller will send to print.
+     * @return the next string to print.
+     */
+    public String getNextPrint();
+
+    /**
+     * Gets all the strings the controller printed.
+     * @return a List containing all printed strings
+     */
+    public List<String> getPrintHistory();
+
+    /**
+     * Prints the next string.
+     * @throws IllegalArgumentException if the next string to print is set as null.
+     */
+    public void printNext();
+
+
 
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,45 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private final List<String> log;
+    private String nextToPrint;
+
+    public SimpleController() {
+        this.log = new LinkedList<>();
+        nextToPrint = null;
+    }
+
+    @Override
+    public void setNextPrint(String newstr) {
+        this.nextToPrint = newstr;
+    }
+
+    @Override
+    public String getNextPrint() {
+        return this.nextToPrint;
+    }
+
+    @Override
+    public List<String> getPrintHistory() {
+        return this.log;
+    }
+
+    @Override
+    public void printNext() {
+        if(this.nextToPrint == null) {
+            throw new IllegalArgumentException("No string to print.");
+        }
+        System.out.println(this.nextToPrint);
+        this.log.add(this.nextToPrint);
+        this.nextToPrint = null;
+    }
 
 }
